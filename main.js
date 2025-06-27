8a06942551f120950d9ef6a5be476396a8c9dff4
UmbertoCarbone <umberto.carb.dev@gmail.com>
2025-06-27 15:50:16 +0200

    al terminale leggiamo il terminale con l'host, eseguito sia http://localhost:3010 che http://localhost:3010/api/blog correttamente!

--- main.js
+++ main.js
@@ -1,1 +1,51 @@
-console.log("blog personale")
+const express = require("express")
+const app = express()
+const port = 3010
+
+app.use(express.static("public"))
+
+app.get("/", (req, res) => {
+    res.send("Server del mio blog!")
+})
+
+app.get("/api/blog", (req, res) => {
+    const dolci = [
+        {
+            titolo: "Ciambellone",
+            contenuto: "asd",
+            immagine: "asad",
+            tags: "wd",
+        },
+        {
+            titolo: "Cracker Barbabietola",
+            contenuto: "asd",
+            immagine: "asd",
+            tags: "asd",
+        },
+        {
+            titolo: "Pane Fritto Dolce",
+            contenuto: "asd",
+            immagine: "asd",
+            tags: "asd",
+        },
+        {
+            titolo: "Pasta Barbabietola",
+            contenuto: "asd",
+            immagine: "asd",
+            tags: "asd",
+        },
+        {
+            titolo: "Torta Paesana",
+            contenuto: "asd",
+            immagine: "",
+            tags: "",
+        },
+    ]
+    console.log(dolci)
+    res.json(dolci)
+})
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port http://localhost:${port}`)
+})
